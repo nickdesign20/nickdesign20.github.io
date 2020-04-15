c4542033e89e38c2e4d1ae0ba7c827137dc55142
nickdesign20 <60147640+nickdesign20@users.noreply.github.com>
2020-04-15 16:31:24 -0400

    Update

---
+++ main.js
@@ -0,0 +1,39 @@
+const onNameClick = function () {
+  //  console.log('got into function')
+  //   $('#message').text('Welcome to the moon.')
+  //   $('#message').css('background-Color', 'blue')
+  //   setTimeout(() => $('#message').text(''), 3000)
+  //   $('.container').addClass('animated rollOut');
+  //   $('#welcomePage').addClass('animated rollIn')
+  }
+
+const addHandlers = () => {
+    // $('#nextView').on('click', onMoonClick)
+  }
+
+  $(() => {
+    addHandlers()
+  })
+
+
+
+
+  // <!-- Button to get back to top of page -->
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+  var height = $(window).scrollTop();
+  if (height > 100) {
+      $('#back2Top').fadeIn();
+  } else {
+      $('#back2Top').fadeOut();
+  }
+});
+$(document).ready(function() {
+  $("#back2Top").click(function(event) {
+      event.preventDefault();
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+      return false;
+  });
+
+});
+/*Scroll to top when arrow up clicked END*/  
